16553dee2be6c68537d45f5b790dc4d9cbf876da
Ahmad Wilson <ahmad.wilson@brigade.com>
2017-11-19 00:02:03 +0000

    add method for extracting a team's record

--- src/nfl/teams.go
+++ src/nfl/teams.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-  "fmt"
-  "golang.org/x/net/html"
-  "net/http"
-  "os"
+	"fmt"
+	"golang.org/x/net/html"
+	// "golang.org/x/net/html/atom"
+	"github.com/yhat/scrape"
+	"net/http"
+	"os"
 )
 
 const base_url = "https://www.pro-football-reference.com"
@@ -13,8 +15,8 @@ const base_url = "https://www.pro-football-reference.com"
 func get_page_hrefs(page_url string) (page_hrefs []string) {
   resp, err := http.Get(page_url)
   if err != nil {
-    fmt.Println("ERROR: Failed to crawl \"" + page_url + "\"")
-    return
+	  fmt.Println("ERROR: Failed to crawl \"" + page_url + "\"")
+	  return
   }
   defer resp.Body.Close()
 
@@ -24,19 +26,44 @@ func get_page_hrefs(page_url string) (page_hrefs []string) {
     switch {
     case tt == html.ErrorToken:
       // End of the document, we're done
-        return
+      return
     case tt == html.StartTagToken:
       t := page_tokens.Token()
 
       isAnchor := t.Data == "a"
       if isAnchor {
-        for _, a := range t.Attr {
-          if a.Key == "href" {
-            page_hrefs = append(page_hrefs, a.Val)
-            break
-          }
-        }
+	for _, a := range t.Attr {
+	  if a.Key == "href" {
+		  page_hrefs = append(page_hrefs, a.Val)
+		  break
+	  }
+	}
       }
+    }
+  }
+
+  return
+}
+
+// get team record for entire season
+func get_team_record(page_url string) (team_record string) {
+  // request and parse the front page
+  resp, err := http.Get(page_url)
+  if err != nil {
+    panic(err)
+  }
+  root, err := html.Parse(resp.Body)
+  if err != nil {
+    panic(err)
+  }
+
+  matcher := func(n *html.Node) bool {
+    return scrape.Attr(n, "data-stat") == "team_record"
+  }
+  x := scrape.FindAllNested(root, matcher)
+  for _, x := range x {
+    if scrape.Text(x) != "" {
+      team_record = scrape.Text(x)
     }
   }
 
@@ -52,6 +79,8 @@ func main() {
 
   hrefs := get_page_hrefs(team_url)
   for _, h := range hrefs {
-    fmt.Println(h)
+	  fmt.Println(h)
   }
+  record := get_team_record(team_url)
+  fmt.Println("Team Record: " + record)
 }
